3c83e984077a9a53851c656646c860c8e8b961b6
sytone <github@sytone.com>
2022-05-27 08:43:58 -0700

    chore: add commit liniting and update hooks

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -74,6 +74,39 @@ yarn lint
 
 Make sure you build, test and lint before pushing to the repository.
 
+## Committing Changes
+
+Conventional Commit format is used by this repository, this is to make the commit process simpler as well as help with the autogeneration of a change log.
+
+### Format
+
+```text
+<type>[(optional <scope>)]: <description>
+
+[optional <body>]
+
+[optional <footer(s)>]
+```
+
+### Types
+
+- build: The commit alters the build system or external dependencies of the product (adding, removing, or upgrading dependencies).
+- change: The commit changes the implementation of an existing feature.
+- chore: The commit includes a technical or preventative maintenance task that is necessary for managing the product or the repository, but it is not tied to any specific feature or user story. For example, releasing the product can be considered a chore. Regenerating generated code that must be included in the repository could be a chore.
+- ci: The commit makes changes to continuous integration or continuous delivery scripts or configuration files.
+- deprecate: The commit deprecates existing functionality, but does not remove it from the product. For example, sometimes older public APIs may get deprecated because newer, more efficient APIs are available. Removing the APIs could break existing integrations so the APIs may be marked as deprecated in order to encourage the integration developers to migrate to the newer APIs while also giving them time before removing older functionality.
+- docs: The commit adds, updates, or revises documentation that is stored in the repository.
+- feat: The commit implements a new feature for the application.
+- fix: The commit fixes a defect in the application.
+- perf: The commit improves the performance of algorithms or general execution time of the product, but does not fundamentally change an existing feature.
+- refactor: The commit refactors existing code in the product, but does not alter or change existing behavior in the product.
+- remove: The commit removes a feature from the product. Typically features are deprecated first for a period of time before being removed. Removing a feature from the product may be considered a breaking change that will require a major version number increment.
+- revert: The commit reverts one or more commits that were previously included in the product, but were accidentally merged or serious issues were discovered that required their removal from the main branch.
+- security: The commit improves the security of the product or resolves a security issue that has been reported.
+- style: The commit updates or reformats the style of the source code, but does not otherwise change the product implementation.
+- test: The commit enhances, adds to, revised, or otherwise changes the suite of automated tests for the product.
+
+
 ## FAQs
 
 ### How does Tasks handle status changes?
--- scripts/Publish-Plugin.ps1
+++ scripts/Publish-Plugin.ps1
@@ -3,7 +3,9 @@ param (
     [Parameter(Mandatory = $true)]
     $Version,
     [Parameter(Mandatory = $true)]
-    $MinimumObsidianVersion
+    $MinimumObsidianVersion,
+    [switch]
+    $DocumentationOnly
 )
 
 [Parameter(Position = 0, Mandatory = $true)]
@@ -15,49 +17,63 @@ if ($pendingChanges -ne '') {
     #exit 1
 }
 
-push-location $PSScriptRoot
-Push-Location ".."
+Push-Location $PSScriptRoot
+Push-Location '..'
 
 Write-Output "Updating to version ${Version} with minimum obsidian version ${MinimumObsidianVersion}"
 
-$publish = Read-Host 'Are you sure you want to continue? (y/n)'
+if ($DocumentationOnly) {
+    $publish = Read-Host 'Update documentation? (y/n)'
+    if ($publish -ieq 'y') {
+        git switch gh-pages
+        git merge main
+        $env:LEFTHOOK = 0
+        git push
+        git switch -
+    }
+} else {
+    $publish = Read-Host 'Update versions in files? (y/n)'
 
-if ($publish -ieq 'y') {
-    Write-Output 'Updating package.json'
-    $packageJson = Get-Content -Path './package.json' | ConvertFrom-Json
-    $packageJson.version = $Version
-    $packageJson | ConvertTo-Json -Depth 10 | Set-Content -Path './package.json'
+    if ($publish -ieq 'y') {
+        Write-Output 'Updating package.json'
+        $packageJson = Get-Content -Path './package.json' | ConvertFrom-Json
+        $packageJson.version = $Version
+        $packageJson | ConvertTo-Json -Depth 100 |
+            ForEach-Object { $_ -replace '(?m)  (?<=^(?:  )*)', '    ' } |
+            Set-Content -Path './package.json'
 
 
-    Write-Output 'Updating manifest.json'
-    $manifestJson = Get-Content -Path './manifest.json' | ConvertFrom-Json
-    $manifestJson.version = $Version
-    $manifestJson.minAppVersion = $MinimumObsidianVersion
-    $manifestJson | ConvertTo-Json -Depth 10 | Set-Content -Path './manifest.json'
+        Write-Output 'Updating manifest.json'
+        $manifestJson = Get-Content -Path './manifest.json' | ConvertFrom-Json
+        $manifestJson.version = $Version
+        $manifestJson.minAppVersion = $MinimumObsidianVersion
+        $manifestJson | ConvertTo-Json -Depth 10 | Set-Content -Path './manifest.json'
 
-    Write-Output 'Updating versions.json'
-    TEMP_FILE=$(mktemp)
-    jq '. += {\'${NEW_VERSION}\": \"${MINIMUM_OBSIDIAN_VERSION}\"}" versions.json > "$TEMP_FILE" || exit 1
-    Move-Item "$TEMP_FILE" versions.json
+        Write-Output 'Updating versions.json'
+        $versionJson = Get-Content -Path './versions.json' | ConvertFrom-Json
+        if (!$versionJson.$Version) {
+            $versionJson | Add-Member -MemberType NoteProperty -Name $Version -Value $MinimumObsidianVersion
+            $versionJson | ConvertTo-Json -Depth 10 | Set-Content -Path './versions.json'
+        }
+    }
 
-    read -p 'Create git commit, tag, and push? [y/N] ' -n 1 -r
-    Write-Output
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-    git add -A .
-    git commit -m"Update to version ${NEW_VERSION}"
-    git tag "${NEW_VERSION}"
-    git push
-    LEFTHOOK=0 git push --tags
-    fi
+    $publish = Read-Host 'Create git commit, tag, and push? (y/n)'
+    if ($publish -ieq 'y') {
+        git add -A
+        git commit -m"Update to version ${Version}"
+        git tag "${Version}"
+        git push
+        $env:LEFTHOOK = 0
+        git push --tags
+    }
 
-    read -p 'Update documentation? [y/N] ' -n 1 -r
-    Write-Output
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-    git switch gh-pages
-    git merge main
-    LEFTHOOK=0 git push
-    git switch -
-    fi
+    $publish = Read-Host 'Update documentation? (y/n)'
+    if ($publish -ieq 'y') {
+        git switch gh-pages
+        git merge main
+        $env:LEFTHOOK = 0
+        git push
+        git switch -
+    }
+
 }
