0716757d833023ff017452745daa06d91f99187f
sytone <github@sytone.com>
2022-06-28 22:46:44 -0700

    fix: 'not done' filter returning all statuses #9

--- src/Query/Filter/StatusField.ts
+++ src/Query/Filter/StatusField.ts
@@ -6,8 +6,8 @@ export class StatusField extends FilterInstructionsBasedField {
     constructor() {
         super();
 
-        this._filters.add('done', (task: Task) => task.status === Status.DONE);
-        this._filters.add('not done', (task: Task) => task.status !== Status.DONE);
+        this._filters.add('done', (task: Task) => task.status.indicator === Status.DONE.indicator);
+        this._filters.add('not done', (task: Task) => task.status.indicator !== Status.DONE.indicator);
     }
 
     protected fieldName(): string {
